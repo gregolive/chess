4db37f45497860cdcad0afc514c526310ac4abd4
Gregory Olive <gregoryolive@outlook.com>
2021-10-26 12:43:06 +0900

    Fixed issue with :location coordinates.

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -14,7 +14,7 @@ class ChessPieces
   def collect_pieces(name, pieces, output = [])
     row = pieces == WHITE ? [6, 7] : [0, 1]
     pieces.each_with_index do |piece, index|
-      location = index < 7 ? [row[0], index] : [row[1], (index - 8)]
+      location = index < 8 ? [row[0], index] : [row[1], (index - 8)]
       info = { label: piece, status: 'alive', owner: name, location: location, moves: nil }
       output.push(info)
     end
