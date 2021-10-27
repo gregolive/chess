1b313fab8d0ed0cb407808d041c95ed6911922a7
Gregory Olive <gregoryolive@outlook.com>
2021-10-28 01:45:48 +0900

    Updated with require database relative and yaml for I/O.

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative '../lib/game'
+require_relative '../lib/database'
 require_relative '../lib/board'
+require_relative '../lib/game'
 require_relative '../lib/pieces'
+require 'yaml'
 
 Game.new
